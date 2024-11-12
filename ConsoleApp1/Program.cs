e4f954842795dc6ce695e9c6cabd13adc1f3f89c
VincentKiwi <vincent.kiwanuka@elev.ga.ntig.se>
2024-11-12 10:46:15 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// if (6 >= 3)
+// {
+// Console.WriteLine("Hello, world");
+// }
+
+
+
+// Console.WriteLine($"whats your name=password");
+// string password = Console.ReadLine().ToLower();
+
+// if (password == "kalleanka")
+// {
+//     Console.WriteLine("yo");
+// }
+// else
+// {
+//     Console.WriteLine("fuck offfff");
+// }
+
+// Console.ReadLine();
+
+
+
